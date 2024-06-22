3d51ec7a0bbb3b06dbfe33057aca3f6b0c197f52
N1191635 <a.pratham1998@gmail.com>
2024-06-22 11:43:09 +0100

    Add example secrets file and update .gitignore
    - Added secrets-example.js to serve as a template for the secrets file.
    - Updated .gitignore to exclude package-lock.json to avoid and dependency conflicts

---
+++ secrets-example.js
@@ -0,0 +1,7 @@
+//FIXME: Add your secrets to this file and then rename this file to secrets.js
+module.exports = {
+    OAUTH_CLIENT: {
+      client_id: 'What you can get on https://console.cloud.google.com/apis/credentials/oauthclient/'
+      client_secret: 'What you can get on https://console.cloud.google.com/apis/credentials/oauthclient/'
+    }
+  }
